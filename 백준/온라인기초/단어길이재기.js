0b5fcdd47493d2edcee886f992436e80e630234a
hae-on <solbi2004@naver.com>
2022-01-13 20:49:01 +0900

    단어 길이 재기

---
+++ 백준/온라인기초/단어길이재기.js
@@ -0,0 +1,7 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('');
+
+const sample = `pulljima`;
+const input = sample.split("");
+
+console.log(input.length);
