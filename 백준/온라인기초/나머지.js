f2a1b8bdcca6d1899d65871aa7f19c65a43888fe
hae-on <solbi2004@naver.com>
2022-01-16 21:15:22 +0900

    나머지

---
+++ 백준/온라인기초/나머지.js
@@ -0,0 +1,14 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const sample = `5 8 4`;
+const input = sample.split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+const C = Number(input[2]);
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
