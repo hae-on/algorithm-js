048de7f221da8b6ed4df24de3b933e87eaa6c59f
hae-on <solbi2004@naver.com>
2022-01-19 20:50:21 +0900

    팩토리얼0의 개수

---
+++ 백준/온라인기초/팩토리얼0의개수.js
@@ -0,0 +1,12 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const input = `10`;
+let n = Number(input);
+
+let cnt = 0;
+while (n > 0) {
+  n = parseInt(n / 5);
+  cnt += n;
+}
+console.log(cnt);
