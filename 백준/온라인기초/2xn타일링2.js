9643541cb5a50e7af1c5ae5099555a5c1a8aec80
hae-on <solbi2004@naver.com>
2022-01-23 20:21:36 +0900

    2xn 타일링2

---
+++ 백준/온라인기초/2xn타일링2.js
@@ -0,0 +1,16 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString();
+
+const input = `8`;
+const n = Number(input);
+
+let dy = Array.from({ length: n + 1 }, () => 0);
+
+dy[1] = 1;
+dy[2] = 3;
+
+for (let i = 3; i <= n; i++) {
+  dy[i] = (dy[i - 2] * 2 + dy[i - 1]) % 10007;
+}
+
+console.log(dy[n]);
