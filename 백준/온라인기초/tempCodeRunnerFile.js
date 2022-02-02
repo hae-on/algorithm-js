14b89f8d94da566c01176761cd3c748ed68e1e96
hae-on <solbi2004@naver.com>
2022-02-02 21:33:01 +0900

    연속합2

--- 백준/온라인기초/tempCodeRunnerFile.js
+++ 백준/온라인기초/tempCodeRunnerFile.js
@@ -1,2 +1,9 @@
-
-  for (let j = 0; j < i; j++) {
+for (let i = 0; i < N; i++) {
+  if (dp[i - 1] + num[i] >= num[i]) {
+    dp[i] = dp[i - 1] + num[i];
+  } else {
+    dp[i] = num[i];
+  }
+}
+console.log(dp);
+console.log(Math.max(...dp));
--- 백준/온라인기초/연속합2.js
+++ 백준/온라인기초/연속합2.js
@@ -9,6 +9,7 @@ const N = Number(input[0]);
 let num = input[1].split(" ").map(Number);
 
 let dp = [];
+let dp2 = [num[0]];
 
 for (let i = 0; i < N; i++) {
   if (dp[i - 1] + num[i] >= num[i]) {
@@ -17,6 +18,16 @@ for (let i = 0; i < N; i++) {
     dp[i] = num[i];
   }
 }
-console.log(dp);
 
-// console.log(Math.max(...dp));
+for (let i = 1; i < N; i++) {
+  if (dp2[i - 1] + num[i] >= dp[i - 1]) {
+    dp2[i] = dp2[i - 1] + num[i];
+  } else {
+    dp2[i] = dp[i - 1];
+  }
+}
+
+let dpMax = Math.max(...dp);
+let dp2Max = Math.max(...dp2);
+
+console.log(Math.max(dpMax, dp2Max));
