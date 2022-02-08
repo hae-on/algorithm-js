20032bae8f3010aa8569d8644b699ef6efae4f29
hae-on <solbi2004@naver.com>
2022-02-08 18:38:08 +0900

    시저 암호

---
+++ 프로그래머스/시저암호.js
@@ -0,0 +1,15 @@
+function solution(s, n) {
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "") answer += " ";
+    else
+      answer += String.fromCharCode(
+        s.charCodeAt(i) > 90
+          ? ((s.charCodeAt(i) + n - 97) % 26) + 97
+          : ((s.charCodeAt(i) + n - 65) % 26) + 65
+      );
+  }
+  return answer;
+}
+
+console.log(solution("AB", 1));
