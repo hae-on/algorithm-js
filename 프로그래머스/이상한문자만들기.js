ae9ba4f4765d9a9eb3f9b44fd921ccf89c516fc9
hae-on <solbi2004@naver.com>
2022-02-08 18:00:37 +0900

    이상한 문자 만들기

---
+++ 프로그래머스/이상한문자만들기.js
@@ -0,0 +1,18 @@
+function solution(s) {
+  return s
+    .split(" ")
+    .map((word) => {
+      let answer = "";
+      for (let i = 0; i < word.length; i++) {
+        if (i % 2) {
+          answer += word[i].toLowerCase();
+        } else {
+          answer += word[i].toUpperCase();
+        }
+      }
+      return answer;
+    })
+    .join(" ");
+}
+
+console.log(solution("try hello world"));
