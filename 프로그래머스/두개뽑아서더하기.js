4f5a0566dfb75d91a4d5139e7daa430edcd5c538
hae-on <solbi2004@naver.com>
2022-02-09 21:11:24 +0900

    두개 뽑아서 더하기

---
+++ 프로그래머스/두개뽑아서더하기.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+  let arr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      arr.push(numbers[i] + numbers[j]);
+    }
+  }
+
+  let answer = [...new Set(arr)];
+  answer.sort((a, b) => a - b);
+  return answer;
+}
+
+console.log(solution([2, 1, 3, 4, 1]));
