9c0091d0fb13fe2dd4e8223c643460e49ff2a14d
hae-on <solbi2004@naver.com>
2022-02-24 13:53:06 +0900

    section2

---
+++ 333/section2/보이는학생.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  let answer = 1;
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      answer++;
+    }
+  }
+  return answer;
+}
+
+console.log(solution([130, 135, 148, 140, 145, 150, 150, 153]));
---
+++ 333/section2/큰수출력하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+  let answer = "";
+  answer += arr[0] + " ";
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) answer += arr[i] + " ";
+  }
+  return answer;
+}
+
+console.log(solution([7, 3, 9, 5, 6, 12]));
