d72779de31287051b2ccf31c2c8487a1961df2a3
hae-on <solbi2004@naver.com>
2022-02-28 17:38:49 +0900

    올바른 괄호, 괄호 문자 제거, 크레인 인형 뽑기

---
+++ 333/section6/괄호문자제거.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  let answer = "";
+  let stack = [];
+  for (let x of s) {
+    if (x === ")") {
+      while (stack.pop() !== "(");
+    } else stack.push(x);
+  }
+  answer = stack.join("");
+  return answer;
+}
+
+console.log(solution("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
