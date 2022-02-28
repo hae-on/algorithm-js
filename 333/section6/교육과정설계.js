28cc40b9b6b12e4c02b7d4f2be9056ecca9a03ec
hae-on <solbi2004@naver.com>
2022-02-28 21:28:01 +0900

    교육 과정 설계

---
+++ 333/section6/교육과정설계.js
@@ -0,0 +1,13 @@
+function solution(need, plan) {
+  let answer = "YES";
+  let queue = need.split("");
+  for (let x of plan) {
+    if (queue.includes(x)) {
+      if (x !== queue.shift()) return "NO";
+    }
+  }
+  if (queue.length > 0) return "NO";
+  return answer;
+}
+
+console.log(solution("CBA", "CBDAGE"));
