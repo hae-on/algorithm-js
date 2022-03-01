ef9151933f899e129ad8130a3abfcbc317882f6e
hae-on <solbi2004@naver.com>
2022-03-01 20:27:06 +0900

    삽입정렬

---
+++ 333/section7/삽입정렬.js
@@ -0,0 +1,15 @@
+function soliution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = arr[i];
+    let j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) arr[j + 1] = arr[j];
+      else break;
+    }
+    arr[j + 1] = tmp;
+  }
+  return answer;
+}
+
+console.log(soliution([11, 7, 5, 6, 10, 9]));
