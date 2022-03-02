088a0307133283506ced40f9d371a4468146f8c9
hae-on <solbi2004@naver.com>
2022-03-02 19:43:19 +0900

    좌표정렬

---
+++ 333/section7/좌표정렬.js
@@ -0,0 +1,18 @@
+function soliution(arr) {
+  let answer = arr;
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+  return answer;
+}
+
+console.log(
+  soliution([
+    [2, 7],
+    [1, 3],
+    [1, 2],
+    [2, 5],
+    [3, 6],
+  ])
+);
