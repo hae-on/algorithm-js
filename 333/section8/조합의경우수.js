03f014ccbe401bbfb3d360b00ed4650d5f59d87b
hae-on <solbi2004@naver.com>
2022-03-07 22:25:39 +0900

    조합의 경우수

---
+++ 333/section8/조합의경우수.js
@@ -0,0 +1,26 @@
+// function solution(n, r) {
+//   let answer = [];
+//   function DFS(n, r) {
+//     if (n === r || r === 0) return 1;
+//     else return DFS(n - 1, r - 1) + DFS(n - 1, r);
+//   }
+//   answer = DFS(n, r);
+//   return answer;
+// }
+
+// console.log(solution(5, 3));
+
+// 복잡한 계산
+function solution(n, r) {
+  let answer = [];
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+  }
+  answer = DFS(n, r);
+  return answer;
+}
+
+console.log(solution(33, 19));
