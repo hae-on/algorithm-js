0a6e1eac90278a89bbd612c7ec3e68fe734adc50
hae-on <solbi2004@naver.com>
2022-03-11 20:15:01 +0900

    동전 교환

---
+++ 333/section10/동전교환.js
@@ -0,0 +1,15 @@
+function solution(m, coin) {
+  let answer = 0;
+  let dy = Array.from({ length: m + 1 }, () => 1000);
+  dy[0] = 0;
+  for (let i = 1; i < coin.length; i++) {
+    for (let j = coin[i]; j <= m; j++) {
+      dy[j] = Math.min(dy[j], dy[j - coin[i]] + 1);
+    }
+  }
+  console.log(dy);
+  answer = dy[m];
+  return answer;
+}
+
+console.log(solution(15, [1, 2, 5]));
