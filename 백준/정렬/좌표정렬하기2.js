7b10bc61a0c0481209d0d1d27e53411706838980
hae-on <solbi2004@naver.com>
2022-03-15 22:35:16 +0900

    좌표 정렬하기 2

---
+++ 백준/정렬/좌표정렬하기2.js
@@ -0,0 +1,19 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const sample = `5
+0 4
+1 2
+1 -1
+2 2
+3 3`;
+const input = sample.split("\n");
+const N = +input.shift();
+const location = input.map((v) => v.split(" ").map(Number));
+
+location.sort((a, b) => {
+  if (a[1] === b[1]) return a[0] - b[0];
+  else return a[1] - b[1];
+});
+
+console.log(location.map((v) => v.join(" ")).join("\n"));
