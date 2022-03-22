803b5a78849486fff02f3c1ba6e062f052bc3273
hae-on <solbi2004@naver.com>
2022-03-22 14:42:38 +0900

    A+B-2

---
+++ 백준/solved/Bronze5/A+B-2.js
@@ -0,0 +1,8 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+
+const sample = `1
+2`;
+const input = sample.split("\n").map(Number);
+
+console.log(input[0] + input[1]);
