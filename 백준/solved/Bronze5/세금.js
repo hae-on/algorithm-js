92fb650a2e6697acf90ae5f63d11cf870f95cc50
hae-on <solbi2004@naver.com>
2022-04-01 21:04:39 +0900

    세금

---
+++ 백준/solved/Bronze5/세금.js
@@ -0,0 +1,8 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const input = `10000000`;
+const num = Number(input);
+
+console.log(num * 0.78);
+console.log(num * 0.8 + num * 0.2 * 0.78);
