92bc66556e28a4f82f55e66a6c9d2056397875f1
hae-on <solbi2004@naver.com>
2022-04-01 20:38:06 +0900

    한글2

---
+++ 백준/solved/Bronze5/한글2.js
@@ -0,0 +1,8 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const input = `가`;
+
+const answer = input.charCodeAt() - 44031;
+
+console.log(answer);
