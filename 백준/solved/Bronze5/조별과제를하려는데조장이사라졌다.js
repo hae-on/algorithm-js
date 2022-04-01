b94e630050702482cd85eeebc8eab20bd7b9fa43
hae-on <solbi2004@naver.com>
2022-04-01 21:09:57 +0900

    조별 과제를 하려는데 조장이 사라졌다

---
+++ 백준/solved/Bronze5/조별과제를하려는데조장이사라졌다.js
@@ -0,0 +1,7 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const input = `12`;
+const num = Number(input);
+
+console.log(Math.ceil(num / 5));
