84d01a460dbdb0dd550b9c46c36e578bef68531f
hae-on <solbi2004@naver.com>
2022-04-03 21:56:16 +0900

    전자레인지

---
+++ 백준/solved/Bronze4/전자레인지.js
@@ -0,0 +1,23 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const input = `100`;
+const T = Number(input);
+const A = 300;
+const B = 60;
+const C = 10;
+
+let answer = "";
+let temp = input;
+
+answer += `${parseInt(temp / A)} `;
+temp = temp % A;
+
+answer += `${parseInt(temp / B)} `;
+temp = temp % B;
+
+answer += `${parseInt(temp / C)}`;
+temp = temp % C;
+
+answer = temp !== 0 ? -1 : answer;
+
+console.log(answer);
