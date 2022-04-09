108595bab55ce40188c4df9c4ba09f73a1dbc17b
hae-on <solbi2004@naver.com>
2022-04-09 22:35:33 +0900

    별찍기5

---
+++ 백준/solved/Bronze3/별찍기5.js
@@ -0,0 +1,12 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim()
+
+const input = `5`;
+const num = Number(input);
+
+let star = "";
+
+for (let i = 1; i <= num; i++) {
+  star += " ".repeat(num - i) + "*".repeat(i * 2 - 1) + "\n";
+}
+
+console.log(star);
