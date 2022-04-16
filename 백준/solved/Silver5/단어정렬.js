08df2ae94387e0e7bd3562bd300a89ae614eeb79
hae-on <solbi2004@naver.com>
2022-04-17 00:09:37 +0900

    단어정렬

---
+++ 백준/solved/Silver5/단어정렬.js
@@ -0,0 +1,27 @@
+const sample = `13
+but
+i
+wont
+hesitate
+no
+more
+no
+more
+it
+cannot
+wait
+im
+yours`;
+const input = sample.split("\n");
+const N = +input.shift();
+
+const answer = [...new Set(input)].sort((a, b) => {
+  if (a.length > b.length) return 1;
+  else if (a.length < b.length) return -1;
+  else {
+    if (a > b) return 1;
+    else return -1;
+  }
+});
+
+console.log(answer.join("\n"));
